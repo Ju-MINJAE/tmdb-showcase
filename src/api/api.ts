fca8654e99eed1293d286d2fae74f8b0cfad68ac
Ju-MINJAE <juminjae98@gmail.com>
2024-11-08 11:33:12 +0900

    Feat: add Infinite scroll

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,13 +2,13 @@ import axios from 'axios';
 const API_BASE_URL = import.meta.env.VITE_TMDB_BASE_URL;
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
-export const getPopularMovies = async () => {
+export const getPopularMovies = async (page = 1) => {
   try {
     const response = await axios.get(`${API_BASE_URL}/movie/popular`, {
       params: {
         api_key: API_KEY,
         language: 'ko-KR',
-        page: 1,
+        page,
       },
     });
     return response.data.results;
--- src/pages/MovieList.tsx
+++ src/pages/MovieList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { getPopularMovies, getTopRatedMovies } from '../api/api';
 import MovieCard from '../components/MovieCard';
 import MovieSwiper from '../components/MovieSwiper';
@@ -14,31 +14,57 @@ interface Movie {
 const MovieList: React.FC = () => {
   const [popularMovies, setPopularMovies] = useState<Movie[]>([]);
   const [topRatedMovies, setTopRatedMovies] = useState<Movie[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const observerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    const fetchPopularMovies = async () => {
-      const popularMovieData = await getPopularMovies();
-      setPopularMovies(popularMovieData);
-    };
-    const fetchNowplayMovies = async () => {
+    const fetchTopRatedMovies = async () => {
       const topRatedMovieData = await getTopRatedMovies();
       setTopRatedMovies(topRatedMovieData);
     };
 
-    fetchPopularMovies();
-    fetchNowplayMovies();
+    fetchTopRatedMovies();
   }, []);
+
+  const fetchPopularMovies = async (page: number) => {
+    setIsLoading(true);
+    const popularMovieData = await getPopularMovies(page);
+    setPopularMovies((prevMovies) => [...prevMovies, ...popularMovieData]);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchPopularMovies(page);
+  }, [page]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && !isLoading) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      },
+      { threshold: 1.0 }
+    );
+
+    if (observerRef.current) observer.observe(observerRef.current);
+
+    return () => {
+      if (observerRef.current) observer.unobserve(observerRef.current);
+    };
+  }, [isLoading]);
 
   return (
     <div className="bg-gradient-to-r from-gray-900 to-gray-800 min-h-screen py-24 px-4 sm:px-6 lg:px-8">
       <h2 className="text-4xl font-bold mb-8 text-center text-white">
-        평점 높은 영화
+        최고 평점 영화
       </h2>
       <MovieSwiper movies={topRatedMovies} />
-      <hr className="mt-6" />
+      <hr className="mt-12" />
       <div className="bg-gradient-to-r from-gray-900 to-gray-800 min-h-screen py-6 px-4 sm:px-6 lg:px-8">
         <h2 className="text-4xl font-bold mb-8 text-center text-white">
-          인기 영화 목록
+          인기 영화 리스트
         </h2>
         <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
           {popularMovies.map((movie) => (
@@ -51,6 +77,10 @@ const MovieList: React.FC = () => {
             </Link>
           ))}
         </div>
+        <div ref={observerRef} className="h-10"></div>
+        {isLoading && (
+          <p className="text-center text-white text-2xl">로딩중....</p>
+        )}
       </div>
     </div>
   );
