1faaedee89b8d1be1d38f74a7307f91c38bd39e1
Ju-MINJAE <juminjae98@gmail.com>
2024-11-08 11:31:44 +0900

    Feat: create Signin, Signup layout

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,5 +1,93 @@
-const Signin = () => {
-  return <div className="mt-24">로그인</div>;
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Eye, EyeOff } from 'lucide-react';
+
+const SignIn = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => setShowPassword(!showPassword);
+
+  return (
+    <div className="w-full mx-auto md:p-8 flex justify-center items-center min-h-screen bg-[#121a29]">
+      <div className="w-full max-w-md bg-white shadow-md rounded-lg p-6">
+        <div className="mb-6">
+          <h1 className="text-2xl font-bold text-center text-gray-800">
+            로그인
+          </h1>
+        </div>
+        <div>
+          <form>
+            <div className="space-y-4">
+              <div className="space-y-2">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  이메일
+                </label>
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="your@email.com"
+                  required
+                  autoComplete="off"
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                />
+              </div>
+              <div className="space-y-2">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  비밀번호
+                </label>
+                <div className="relative">
+                  <input
+                    id="password"
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder="••••••••"
+                    required
+                    className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                  />
+                  <button
+                    type="button"
+                    className="absolute right-0 top-0 h-full px-3 py-2 text-gray-600 hover:text-gray-900"
+                    onClick={togglePasswordVisibility}
+                    aria-label={
+                      showPassword ? '비밀번호 숨기기' : '비밀번호 보기'
+                    }
+                  >
+                    {showPassword ? (
+                      <EyeOff className="h-5 w-5" />
+                    ) : (
+                      <Eye className="h-5 w-5" />
+                    )}
+                  </button>
+                </div>
+              </div>
+            </div>
+            <button
+              className="w-full mt-6 px-4 py-2 bg-[#28344a] text-white font-semibold rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2"
+              type="submit"
+            >
+              로그인
+            </button>
+          </form>
+        </div>
+        <div className="mt-6 flex flex-col space-y-2 text-center text-sm">
+          <Link to="/forgot-password" className="text-blue-600 hover:underline">
+            비밀번호를 잊으셨나요?
+          </Link>
+          <div>
+            계정이 없으신가요?{' '}
+            <Link to="/signup" className="text-blue-600 hover:underline">
+              회원가입
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Signin;
+export default SignIn;
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,7 +1,145 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Eye, EyeOff } from 'lucide-react';
+
 const Signup = () => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [showConfirmPassword, setConfirmShowPassword] =
+    useState<boolean>(false);
+
+  const togglePasswordVisibility = () => setShowPassword(!showPassword);
+  const toggleConfirmPasswordVisibility = () =>
+    setConfirmShowPassword(!showPassword);
+
   return (
-    <div className="mt-24">
-      <h1>dqwkel;qwk;dlkqwl;dqw</h1>
+    <div className="w-full mx-auto md:p-8 flex justify-center items-center min-h-screen bg-[#121a29]">
+      <div className="w-full max-w-md bg-white shadow-md rounded-lg p-6">
+        <div className="mb-6">
+          <h1 className="text-2xl font-bold text-center text-gray-800">
+            회원가입
+          </h1>
+        </div>
+        <div>
+          <form>
+            <div className="space-y-4">
+              <div className="space-y-2">
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  이름
+                </label>
+                <input
+                  id="name"
+                  type="text"
+                  placeholder="홍길동"
+                  required
+                  autoComplete="off"
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                />
+              </div>
+
+              <div className="space-y-2">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  이메일
+                </label>
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="your@email.com"
+                  required
+                  autoComplete="off"
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                />
+              </div>
+
+              <div className="space-y-2">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  비밀번호
+                </label>
+                <div className="relative">
+                  <input
+                    id="password"
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder="••••••••"
+                    required
+                    className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                  />
+                  <button
+                    type="button"
+                    className="absolute right-0 top-0 h-full px-3 py-2 text-gray-600 hover:text-gray-900"
+                    onClick={togglePasswordVisibility}
+                    aria-label={
+                      showPassword ? '비밀번호 숨기기' : '비밀번호 보기'
+                    }
+                  >
+                    {showPassword ? (
+                      <EyeOff className="h-5 w-5" />
+                    ) : (
+                      <Eye className="h-5 w-5" />
+                    )}
+                  </button>
+                </div>
+              </div>
+
+              <div className="space-y-2">
+                <label
+                  htmlFor="confirmPassword"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  비밀번호 확인
+                </label>
+                <div className="relative">
+                  <input
+                    id="confirmPassword"
+                    type={showConfirmPassword ? 'text' : 'password'}
+                    placeholder="••••••••"
+                    required
+                    className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                  />
+                  <button
+                    type="button"
+                    className="absolute right-0 top-0 h-full px-3 py-2 text-gray-600 hover:text-gray-900"
+                    onClick={toggleConfirmPasswordVisibility}
+                    aria-label={
+                      showConfirmPassword ? '비밀번호 숨기기' : '비밀번호 보기'
+                    }
+                  >
+                    {showPassword ? (
+                      <EyeOff className="h-5 w-5" />
+                    ) : (
+                      <Eye className="h-5 w-5" />
+                    )}
+                  </button>
+                </div>
+              </div>
+            </div>
+            <button
+              className="w-full mt-6 px-4 py-2 bg-[#28344a] text-white font-semibold rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2"
+              type="submit"
+            >
+              로그인
+            </button>
+          </form>
+        </div>
+        <div className="mt-6 flex flex-col space-y-2 text-center text-sm">
+          <Link to="/forgot-password" className="text-blue-600 hover:underline">
+            비밀번호를 잊으셨나요?
+          </Link>
+          <div>
+            이미 계정이 있으신가요?{' '}
+            <Link to="/signin" className="text-blue-600 hover:underline">
+              회원가입
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
